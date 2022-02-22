dfb9da34d7dd315562b2f714a72ecdb4ce61de0f
David Garcia <david.garcia@canonical.com>
2022-02-22 12:26:40 +0100

    Add full licence to lib

--- lib/charms/kafka_k8s/v0/kafka.py
+++ lib/charms/kafka_k8s/v0/kafka.py
@@ -1,3 +1,19 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Kafka library.
 
 This [library](https://juju.is/docs/sdk/libraries) implements both sides of the
@@ -80,7 +96,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 2
+LIBPATCH = 3
 
 
 KAFKA_HOST_APP_KEY = "host"
