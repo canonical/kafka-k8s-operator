16131b19bafa8304d58d84c3534663cc68f56b47
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2022-08-08 00:21:09 +0100

    test: fix zookeeper name

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -13,13 +13,12 @@ logger = logging.getLogger(__name__)
 APP_NAME = "kafka"
 ZK = "zookeeper-k8s"
 
+
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest):
     kafka_charm = await ops_test.build_charm(".")
     await asyncio.gather(
-        ops_test.model.deploy(
-            "zookeeper", channel="edge", application_name=ZK, num_units=1
-        ),
+        ops_test.model.deploy("zookeeper-k8s", channel="edge", application_name=ZK, num_units=1),
         ops_test.model.deploy(kafka_charm, application_name=APP_NAME, num_units=1),
     )
     await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK])
@@ -30,6 +29,7 @@ async def test_build_and_deploy(ops_test: OpsTest):
     await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK])
     assert ops_test.model.applications[APP_NAME].status == "active"
     assert ops_test.model.applications[ZK].status == "active"
+
 
 @pytest.mark.abort_on_fail
 async def test_blocks_without_zookeeper(ops_test: OpsTest):
