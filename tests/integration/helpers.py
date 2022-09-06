7ea68fcb07e741b32a016f808dcca0bb84a1236a
Raul Zamora <raul.zamora@canonical.com>
2022-09-06 11:20:52 +0200

    add rotation test

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -4,12 +4,14 @@
 import re
 from pathlib import Path
 from subprocess import PIPE, check_output
-from typing import Any, List, Tuple
+from typing import Any, Dict, List, Tuple
 
 import yaml
+from pytest_operator.plugin import OpsTest
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
+ZK_NAME = "zookeeper-k8s"
 
 
 def check_user(model_full_name: str, username: str, zookeeper_uri: str) -> None:
@@ -21,6 +23,19 @@ def check_user(model_full_name: str, username: str, zookeeper_uri: str) -> None:
     )
 
     assert "SCRAM-SHA-512" in result
+
+
+def get_user(model_full_name: str, username: str, zookeeper_uri: str) -> None:
+    """Get information related to a user stored on zookeeper."""
+    container_command = f"KAFKA_OPTS=-Djava.security.auth.login.config=/data/kafka/config/kafka-jaas.cfg ./opt/kafka/bin/kafka-configs.sh --zookeeper {zookeeper_uri} --describe --entity-type users --entity-name {username}"
+    result = check_output(
+        f"JUJU_MODEL={model_full_name} juju ssh --container kafka kafka-k8s/0 '{container_command}'",
+        stderr=PIPE,
+        shell=True,
+        universal_newlines=True,
+    )
+
+    return result
 
 
 def show_unit(unit_name: str, model_full_name: str) -> Any:
@@ -53,3 +68,31 @@ def get_zookeeper_connection(unit_name: str, model_full_name: str) -> Tuple[List
         return usernames, zookeeper_uri
     else:
         raise Exception("config not found")
+
+
+def get_kafka_zk_relation_data(unit_name: str, model_full_name: str) -> Dict[str, str]:
+    result = show_unit(unit_name=unit_name, model_full_name=model_full_name)
+    relations_info = result[unit_name]["relation-info"]
+
+    zk_relation_data = {}
+    for info in relations_info:
+        if info["endpoint"] == "zookeeper":
+            zk_relation_data["chroot"] = info["application-data"]["chroot"]
+            zk_relation_data["endpoints"] = info["application-data"]["endpoints"]
+            zk_relation_data["password"] = info["application-data"]["password"]
+            zk_relation_data["uris"] = info["application-data"]["uris"]
+            zk_relation_data["username"] = info["application-data"]["username"]
+    return zk_relation_data
+
+
+async def set_password(ops_test: OpsTest, username="sync", password=None, num_unit=0) -> str:
+    """Use the charm action to start a password rotation."""
+    params = {"username": username}
+    if password:
+        params["password"] = password
+
+    action = await ops_test.model.units.get(f"{APP_NAME}/{num_unit}").run_action(
+        "set-password", **params
+    )
+    password = await action.wait()
+    return password.results
---
+++ tests/integration/test_password_rotation.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import asyncio
+import logging
+
+import pytest
+from helpers import (
+    APP_NAME,
+    ZK_NAME,
+    get_kafka_zk_relation_data,
+    get_user,
+    set_password,
+)
+from pytest_operator.plugin import OpsTest
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.abort_on_fail
+@pytest.mark.skip_if_deployed
+async def test_build_and_deploy(ops_test: OpsTest):
+    kafka_charm = await ops_test.build_charm(".")
+    await asyncio.gather(
+        ops_test.model.deploy(ZK_NAME, channel="edge", application_name=ZK_NAME, num_units=3),
+        ops_test.model.deploy(kafka_charm, application_name=APP_NAME, num_units=1),
+    )
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications[ZK_NAME].units) == 3)
+    await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK_NAME])
+    assert ops_test.model.applications[APP_NAME].status == "waiting"
+    assert ops_test.model.applications[ZK_NAME].status == "active"
+
+    await ops_test.model.add_relation(APP_NAME, ZK_NAME)
+
+    async with ops_test.fast_forward():
+        await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK_NAME])
+
+    assert ops_test.model.applications[APP_NAME].status == "active"
+    assert ops_test.model.applications[ZK_NAME].status == "active"
+
+
+async def test_password_rotation(ops_test: OpsTest):
+    """Check that password stored on ZK has changed after a password rotation."""
+    relation_data = get_kafka_zk_relation_data(
+        unit_name=f"{APP_NAME}/0", model_full_name=ops_test.model_full_name
+    )
+    uri = relation_data["uris"].split(",")[-1]
+
+    initial_sync_user = get_user(
+        username="sync",
+        zookeeper_uri=uri,
+        model_full_name=ops_test.model_full_name,
+    )
+
+    result = await set_password(ops_test, username="sync", num_unit=0)
+    assert "sync-password" in result.keys()
+
+    await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK_NAME])
+
+    new_sync_user = get_user(
+        username="sync",
+        zookeeper_uri=uri,
+        model_full_name=ops_test.model_full_name,
+    )
+
+    assert initial_sync_user != new_sync_user
