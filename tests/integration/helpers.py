9026d420fd86b4ee12c556668c28114b762d5420
Raul Zamora <raul.zamora@canonical.com>
2022-09-06 15:34:43 +0200

    fix integration tests

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -9,12 +9,15 @@ from typing import Any, List, Tuple
 import yaml
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+KAFKA_CONTAINER = METADATA["resources"]["kafka-image"]["upstream-source"]
 APP_NAME = METADATA["name"]
+ZK_NAME = "zookeeper-k8s"
 
 
 def check_user(model_full_name: str, username: str, zookeeper_uri: str) -> None:
+    container_command = f"KAFKA_OPTS=-Djava.security.auth.login.config=/data/kafka/config/kafka-jaas.cfg ./opt/kafka/bin/kafka-configs.sh --zookeeper {zookeeper_uri} --describe --entity-type users --entity-name {username}"
     result = check_output(
-        f"JUJU_MODEL={model_full_name} juju ssh kafka-k8s/0 'kafka.configs --zookeeper {zookeeper_uri} --describe --entity-type users --entity-name {username}'",
+        f"JUJU_MODEL={model_full_name} juju ssh --container kafka kafka-k8s/0 '{container_command}'",
         stderr=PIPE,
         shell=True,
         universal_newlines=True,
--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -6,33 +6,48 @@ import asyncio
 import logging
 
 import pytest
+from helpers import APP_NAME, KAFKA_CONTAINER, ZK_NAME
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
-
-APP_NAME = "kafka"
-ZK = "zookeeper-k8s"
 
 
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest):
     kafka_charm = await ops_test.build_charm(".")
     await asyncio.gather(
-        ops_test.model.deploy("zookeeper-k8s", channel="edge", application_name=ZK, num_units=1),
-        ops_test.model.deploy(kafka_charm, application_name=APP_NAME, num_units=1),
+        ops_test.model.deploy(
+            "zookeeper-k8s",
+            channel="edge",
+            application_name=ZK_NAME,
+            num_units=3,
+        ),
+        ops_test.model.deploy(
+            kafka_charm,
+            application_name=APP_NAME,
+            num_units=1,
+            resources={"kafka-image": KAFKA_CONTAINER},
+        ),
     )
-    await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK])
-    assert ops_test.model.applications[APP_NAME].status == "waiting"
-    assert ops_test.model.applications[ZK].status == "active"
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications[ZK_NAME].units) == 3)
+    await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK_NAME], timeout=1000)
 
-    await ops_test.model.add_relation(APP_NAME, ZK)
-    await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK])
+    assert ops_test.model.applications[APP_NAME].status == "waiting"
+    assert ops_test.model.applications[ZK_NAME].status == "active"
+
+    await ops_test.model.add_relation(APP_NAME, ZK_NAME)
+
+    async with ops_test.fast_forward():
+        await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK_NAME])
+
     assert ops_test.model.applications[APP_NAME].status == "active"
-    assert ops_test.model.applications[ZK].status == "active"
+    assert ops_test.model.applications[ZK_NAME].status == "active"
 
 
 @pytest.mark.abort_on_fail
 async def test_blocks_without_zookeeper(ops_test: OpsTest):
-    await asyncio.gather(ops_test.model.applications[ZK].remove())
+    await asyncio.gather(ops_test.model.applications[ZK_NAME].remove())
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications[ZK_NAME].units) == 0)
     await ops_test.model.wait_for_idle(apps=[APP_NAME])
-    assert ops_test.model.applications[ZK].status == "blocked"
+
+    assert ops_test.model.applications[APP_NAME].status == "blocked"
