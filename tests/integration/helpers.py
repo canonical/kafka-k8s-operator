55c9317382cb5d22c2ce243d34583eddaaad7767
Raúl Zamora Martínez <76525382+zmraul@users.noreply.github.com>
2022-09-07 13:20:02 +0200

    Fix integration tests (#14)
    * fix integration tests
    
    Co-authored-by: Marc Oppenheimer <marcaoppenheimer@gmail.com>

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -7,14 +7,18 @@ from subprocess import PIPE, check_output
 from typing import Any, List, Tuple
 
 import yaml
+from pytest_operator.plugin import OpsTest
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+KAFKA_CONTAINER = METADATA["resources"]["kafka-image"]["upstream-source"]
 APP_NAME = METADATA["name"]
+ZK_NAME = "zookeeper-k8s"
 
 
 def check_user(model_full_name: str, username: str, zookeeper_uri: str) -> None:
+    container_command = f"KAFKA_OPTS=-Djava.security.auth.login.config=/data/kafka/config/kafka-jaas.cfg ./opt/kafka/bin/kafka-configs.sh --zookeeper {zookeeper_uri} --describe --entity-type users --entity-name {username}"
     result = check_output(
-        f"JUJU_MODEL={model_full_name} juju ssh kafka-k8s/0 'kafka.configs --zookeeper {zookeeper_uri} --describe --entity-type users --entity-name {username}'",
+        f"JUJU_MODEL={model_full_name} juju ssh --container kafka kafka-k8s/0 '{container_command}'",
         stderr=PIPE,
         shell=True,
         universal_newlines=True,
@@ -53,3 +57,22 @@ def get_zookeeper_connection(unit_name: str, model_full_name: str) -> Tuple[List
         return usernames, zookeeper_uri
     else:
         raise Exception("config not found")
+
+
+def check_application_status(ops_test: OpsTest, app_name: str) -> str:
+    """Return the application status for an app name."""
+    model_name = ops_test.model.info.name
+    proc = check_output(f"juju status --model={model_name}".split())
+    proc = proc.decode("utf-8")
+
+    statuses = {"active", "maintenance", "waiting", "blocked"}
+    for line in proc.splitlines():
+        parts = line.split()
+        # first line with app name will have application status
+        if parts and parts[0] == app_name:
+            # NOTE: intersects possible statuses with the list of values:
+            # this is done because sometimes version number exists and
+            # sometimes it doesn't on juju status output
+            find_status = list(statuses & set(parts))
+            if find_status:
+                return find_status[0]
