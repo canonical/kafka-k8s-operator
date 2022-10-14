d3a3bb7967cb421001408a9a80223fde190bb89f
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2022-10-14 13:31:14 +0100

    chore: defer cert events writing to non-connecting container (for bundles)

--- src/tls.py
+++ src/tls.py
@@ -91,6 +91,10 @@ class KafkaTLS(Object):
             event.defer()
             return
 
+        if not self.charm.container.can_connect():
+            event.defer()
+            return
+
         # avoid setting tls files and restarting
         if event.certificate_signing_request != self.csr:
             logger.error("Can't use certificate, found unknown CSR")
