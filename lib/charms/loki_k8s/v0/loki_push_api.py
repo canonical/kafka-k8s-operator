9f1f46a723c624bfb179761a4c3b830a4289ad49
Enrico Deusebio <edeusebio85@gmail.com>
2023-03-18 01:41:19 +0100

    [MISC] General update charm libs

--- lib/charms/loki_k8s/v0/loki_push_api.py
+++ lib/charms/loki_k8s/v0/loki_push_api.py
@@ -449,7 +449,6 @@ import socket
 import subprocess
 import tempfile
 import typing
-import uuid
 from copy import deepcopy
 from gzip import GzipFile
 from hashlib import sha256
@@ -484,7 +483,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 15
+LIBPATCH = 17
 
 logger = logging.getLogger(__name__)
 
@@ -1241,15 +1240,20 @@ class LokiPushApiProvider(Object):
             url: An optional url value to update relation data.
             relation: An optional instance of `class:ops.model.Relation` to update.
         """
+        # if no relation is specified update all of them
         if not relation:
-            if not self._charm.model.get_relation(self._relation_name):
+            if not self._charm.model.relations.get(self._relation_name):
                 return
 
-            relation = self._charm.model.get_relation(self._relation_name)
+            relations_list = self._charm.model.relations.get(self._relation_name)
+        else:
+            relations_list = [relation]
 
         endpoint = self._endpoint(url or self._url)
 
-        relation.data[self._charm.unit].update({"endpoint": json.dumps(endpoint)})
+        for relation in relations_list:
+            relation.data[self._charm.unit].update({"endpoint": json.dumps(endpoint)})
+
         logger.debug("Saved endpoint in unit relation data")
 
     @property
@@ -2386,11 +2390,9 @@ class CosTool:
             #         expr: up
             transformed_rules = {"groups": []}  # type: ignore
             for rule in rules["groups"]:
-                transformed = {"name": str(uuid.uuid4()), "rules": [rule]}
-                transformed_rules["groups"].append(transformed)
+                transformed_rules["groups"].append(rule)
 
             rule_path.write_text(yaml.dump(transformed_rules))
-
             args = [str(self.path), "--format", "logql", "validate", str(rule_path)]
             # noinspection PyBroadException
             try:
