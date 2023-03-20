cb0356c0f761844d0c61244245d3a12f7f737745
Paolo Sottovia <paolo.sottovia@canonical.com>
2023-03-20 23:33:05 +0100

    wip

--- src/literals.py
+++ src/literals.py
@@ -4,6 +4,10 @@
 
 """Literals used by the Kafka K8s charm."""
 
+from dataclasses import dataclass
+from typing import Dict, Literal
+
+
 CHARM_KEY = "kafka-k8s"
 ZK_NAME = "zookeeper-k8s"
 PEER = "cluster"
@@ -16,3 +20,22 @@ STORAGE = "log-data"
 DATA_DIR = "/data/kafka"
 LOG_DIR = "/logs/kafka"
 JMX_EXPORTER_PORT = 9101
+
+INTER_BROKER_USER = "sync"
+ADMIN_USER = "admin"
+
+AuthMechanism = Literal["SASL_PLAINTEXT", "SASL_SSL", "SSL"]
+Scope = Literal["INTERNAL", "CLIENT"]
+
+
+@dataclass
+class Ports:
+    client: int
+    internal: int
+
+
+SECURITY_PROTOCOL_PORTS: Dict[AuthMechanism, Ports] = {
+    "SASL_PLAINTEXT": Ports(9092, 19092),
+    "SASL_SSL": Ports(9093, 19093),
+    "SSL": Ports(9094, 19094),
+}
