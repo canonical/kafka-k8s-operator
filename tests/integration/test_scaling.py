4c558435255cb0f0c13acb35363f1eff17741a6b
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2024-04-28 18:31:19 +0100

    test: working scaling

--- tests/integration/test_scaling.py
+++ tests/integration/test_scaling.py
@@ -4,7 +4,6 @@
 
 import asyncio
 import logging
-import time
 
 import pytest
 from pytest_operator.plugin import OpsTest
@@ -12,9 +11,7 @@ from pytest_operator.plugin import OpsTest
 from .helpers import (
     APP_NAME,
     KAFKA_CONTAINER,
-    KAFKA_SERIES,
     ZK_NAME,
-    ZK_SERIES,
     get_active_brokers,
     get_address,
     get_kafka_zk_relation_data,
@@ -28,15 +25,12 @@ async def test_kafka_simple_scale_up(ops_test: OpsTest):
     kafka_charm = await ops_test.build_charm(".")
 
     await asyncio.gather(
-        ops_test.model.deploy(
-            ZK_NAME, channel="edge", application_name=ZK_NAME, num_units=1, series=ZK_SERIES
-        ),
+        ops_test.model.deploy(ZK_NAME, channel="3/edge", application_name=ZK_NAME, num_units=1),
         ops_test.model.deploy(
             kafka_charm,
             application_name=APP_NAME,
             num_units=1,
             resources={"kafka-image": KAFKA_CONTAINER},
-            series=KAFKA_SERIES,
         ),
     )
     await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK_NAME])
@@ -47,9 +41,6 @@ async def test_kafka_simple_scale_up(ops_test: OpsTest):
             apps=[APP_NAME, ZK_NAME], timeout=1000, status="active", idle_period=20
         )
 
-    assert ops_test.model.applications[ZK_NAME].status == "active"
-    assert ops_test.model.applications[APP_NAME].status == "active"
-
     await ops_test.model.applications[APP_NAME].scale(scale=3)
     await ops_test.model.block_until(lambda: len(ops_test.model.applications[APP_NAME].units) == 3)
     await ops_test.model.wait_for_idle(
@@ -57,7 +48,9 @@ async def test_kafka_simple_scale_up(ops_test: OpsTest):
     )
 
     kafka_zk_relation_data = get_kafka_zk_relation_data(
-        unit_name=f"{APP_NAME}/0", model_full_name=ops_test.model_full_name
+        ops_test=ops_test,
+        unit_name=f"{APP_NAME}/2",
+        owner=ZK_NAME,
     )
 
     # can't use *-endpoints address from outside of K8s cluster, need to patch
@@ -75,13 +68,18 @@ async def test_kafka_simple_scale_up(ops_test: OpsTest):
 @pytest.mark.abort_on_fail
 async def test_kafka_simple_scale_down(ops_test: OpsTest):
     await ops_test.model.applications[APP_NAME].scale(scale=2)
-    await ops_test.model.block_until(lambda: len(ops_test.model.applications[APP_NAME].units) == 2)
-    await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000)
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME], status="active", timeout=1000, idle_period=30, wait_for_exact_units=2
+    )
 
-    time.sleep(30)
+    # ensuring ZK data gets updated
+    async with ops_test.fast_forward(fast_interval="20s"):
+        await asyncio.sleep(60)
 
     kafka_zk_relation_data = get_kafka_zk_relation_data(
-        unit_name=f"{APP_NAME}/0", model_full_name=ops_test.model_full_name
+        ops_test=ops_test,
+        unit_name=f"{APP_NAME}/1",
+        owner=ZK_NAME,
     )
 
     # can't use *-endpoints address from outside of K8s cluster, need to patch
