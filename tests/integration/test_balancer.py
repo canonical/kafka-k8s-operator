346aa2f7d039fd4265d2cef37570a996008680d3
Alex Batisse <alex.batisse@canonical.com>
2024-08-24 09:52:25 +0200

    WIP: Re-add balancer_is_ready check

--- tests/integration/test_balancer.py
+++ tests/integration/test_balancer.py
@@ -231,6 +231,7 @@ class TestBalancer:
             if await unit.is_leader_from_status():
                 leader_unit = unit
 
+        assert balancer_is_ready(ops_test=ops_test, app_name=self.balancer_app)
         rebalance_action = await leader_unit.run_action(
             "rebalance", mode="add", brokerid=new_broker_id, dryrun=False
         )
