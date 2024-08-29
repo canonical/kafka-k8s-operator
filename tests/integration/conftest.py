07fb518f594ef6947ee496df2103692043514c5d
Alex Batisse <alex.batisse@canonical.com>
2024-08-29 09:53:43 +0200

    ci: Use dp-workflow build override instead of custom fixture

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -2,8 +2,6 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import os
-from pathlib import Path
 
 import pytest
 from pytest_operator.plugin import OpsTest
@@ -17,11 +15,6 @@ def usernames():
 @pytest.fixture(scope="module")
 async def kafka_charm(ops_test: OpsTest):
     """Kafka charm used for integration testing."""
-    in_ci = os.environ.get("CI", None) is not None
-    local_charm = next(iter(Path(".").glob("*.charm")), None)
-    if in_ci and local_charm is not None:
-        return local_charm.absolute()
-
     charm = await ops_test.build_charm(".")
     return charm
 
