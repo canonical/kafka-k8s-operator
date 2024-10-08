4a63963dd07a260c56b4d33a4f0f232fdd830de4
Alex Batisse <alex.batisse@canonical.com>
2024-10-08 15:43:27 +0200

    tests: Migrate test_charm to scenario

--- tests/unit/_test_charm.py
+++ tests/unit/_test_charm.py
@@ -528,7 +528,9 @@ def test_zookeeper_broken_stops_service_and_removes_meta_properties(harness: Har
         harness.remove_relation(zk_rel_id)
 
         patched_stop_snap_service.assert_called_once()
-        assert re.match(r"rm .*/meta.properties", " ".join(patched_exec.call_args_list[1].args[0]))
+        assert re.findall(
+            r"meta.properties -delete", " ".join(patched_exec.call_args_list[1].args[0])
+        )
         assert isinstance(harness.charm.unit.status, BlockedStatus)
 
 
