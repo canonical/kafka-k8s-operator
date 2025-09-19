ba28c414a5be081bbf0f23833842e407f8118bb3
Vladimir <48120135+izmalk@users.noreply.github.com>
2025-09-19 21:06:14 +0100

    Add a cookie consent request
    Asking for cookie-usage consent to use Google analytics for docs

--- docs/conf.py
+++ docs/conf.py
@@ -198,7 +198,7 @@ sitemap_show_lastmod = True
 #######################
 
 html_static_path = ["_static"]
-#templates_path = ["_templates"]
+templates_path = ["_templates"]
 
 
 #############
@@ -301,13 +301,15 @@ exclude_patterns = [
 
 # Adds custom CSS files, located under 'html_static_path'
 
-# html_css_files = []
-
+html_css_files = [
+	'cookie-banner.css'
+]
 
 # Adds custom JavaScript files, located under 'html_static_path'
 
-# html_js_files = []
-
+html_js_files = [
+	'js/bundle.js',
+]
 
 # Specifies a reST snippet to be appended to each .rst file
 
