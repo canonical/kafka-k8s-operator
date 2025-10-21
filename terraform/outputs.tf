f07bc92539b4ecca955a0fdc22430bb3ae064f83
Iman Enami <44609233+imanenami@users.noreply.github.com>
2025-10-21 08:57:14 +0400

    [DPE-8310] feat: add Terraform charm module (#220)

---
+++ terraform/outputs.tf
@@ -0,0 +1,25 @@
+# CC006 mandatory outputs
+output "app_name" {
+  description = "Name of the main Kafka application"
+  value       = juju_application.kafka.name
+}
+
+output "provides_endpoints" {
+  description = "Relation endpoints this charm provides"
+  value = [
+    "kafka-client",
+    "cos-agent",
+  ]
+}
+
+output "offers" {
+  description = "offers created by this charm"
+  value = {
+    kafka-client = juju_offer.kafka_client[*].url
+  }
+}
+
+output "kafka_client_offer" {
+  description = "Kafka client offer URL for external applications"
+  value       = juju_offer.kafka_client[*].url
+}
