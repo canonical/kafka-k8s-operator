1a0051ae11474ca6733a2c4a52c28590795d3445
Vladimir Izmalkov <48120135+izmalk@users.noreply.github.com>
2026-02-25 10:47:06 +0000

    Add a cookie banner (new version) (#255)

--- docs/conf.py
+++ docs/conf.py
@@ -191,8 +191,12 @@ else:
 
 # Template and asset locations
 
-#html_static_path = ["_static"]
-#templates_path = ["_templates"]
+html_static_path = [
+    ".sphinx/_static",
+]
+templates_path = [
+    ".sphinx/_templates",
+]
 
 
 #############
@@ -283,13 +287,15 @@ exclude_patterns = [
 
 # Adds custom CSS files, located under 'html_static_path'
 
-# html_css_files = []
-
+html_css_files = [
+    "cookie-banner.css",
+]
 
 # Adds custom JavaScript files, located under 'html_static_path'
 
-# html_js_files = []
-
+html_js_files = [
+    "bundle.js",
+]
 
 # Specifies a reST snippet to be appended to each .rst file
 
